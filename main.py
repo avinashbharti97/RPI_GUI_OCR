db384e297a0891859ef99cf8b491019b7f731351
avinashbharti97 <avinashbharti97@gmail.com>
2020-04-18 23:34:56 +0530

    capturing photo function added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter
 import cv2
 import PIL.Image, PIL.ImageTk
+import time
 
 class App:
     def __init__(self, window, window_title, video_source=0):
@@ -14,6 +15,9 @@ class App:
         #canvas for video render
         self.canvas = tkinter.Canvas(window, width=self.vid.width, height=self.vid.height)
         self.canvas.pack()
+        
+        self.btn_snapshot = tkinter.Button(window, text="Snapshot", width=50, command=self.snapshot)
+        self.btn_snapshot.pack(anchor=tkinter.CENTER, expand=True)
 
         self.delay = 15
         self.update()
@@ -29,6 +33,13 @@ class App:
             self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
             self.canvas.create_image(0,0, image = self.photo, anchor=tkinter.NW)
         self.window.after(self.delay, self.update)
+
+    def snapshot(self):
+        #get a frame from video source
+        ret, frame = self.vid.get_frame()
+
+        if ret:
+            cv2.imwrite("frame-"+time.strftime("%d-%m-%Y-%H-%M-%S")+".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
 
 class VideoCapture:
     def __init__(self, video_source=0):
