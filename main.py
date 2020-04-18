effc2a32786bc90112b3d1e23fccdb61ec003fe0
avinashbharti97 <avinashbharti97@gmail.com>
2020-04-18 23:10:15 +0530

    camera input donw

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
 import tkinter
 import cv2
+import PIL.Image, PIL.ImageTk
 
 class App:
-    def __init__(self, window, window_title):
+    def __init__(self, window, window_title, video_source=0):
         self.window = window
         self.window.title = window_title
+        self.video_source = video_source
+
+        #open vid source
+        self.vid = VideoCapture(video_source)
+
+        #canvas for video render
+        self.canvas = tkinter.Canvas(window, width=self.vid.width, height=self.vid.height)
+        self.canvas.pack()
+
+        self.delay = 15
+        self.update()
 
         self.window.mainloop()
+
+    #loop to render frames continuosuly
+    def update(self):
+
+        ret, frame = self.vid.get_frame()
+
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+            self.canvas.create_image(0,0, image = self.photo, anchor=tkinter.NW)
+        self.window.after(self.delay, self.update)
+
+class VideoCapture:
+    def __init__(self, video_source=0):
+        #Open the video source
+        self.vid = cv2.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source", video_source)
+
+        #get the width and height of vid source
+        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    #Release the video source when the object is destroyed
+    def __del__(self):
+        if self.vid.isOpened():
+            self.vid.release()
+
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                #this return a boolean success flag and the frame converted to BGR
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return (ret, None)
 
 App(tkinter.Tk(), "OCR Reader")
