f4474b6624d72d3cd6a5ae1f453805e2f31b1646
avinashbharti97 <avinashbharti97@gmail.com>
2020-04-19 00:11:19 +0530

    basic tessarct with output box added

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import tkinter
 import cv2
 import PIL.Image, PIL.ImageTk
 import time
+import pytesseract
+from tkinter.scrolledtext import ScrolledText 
+import tkinter.ttk as ttk
+from ttkthemes import ThemedStyle
+
 
 class App:
     def __init__(self, window, window_title, video_source=0):
         self.window = window
         self.window.title = window_title
+        self.style = ThemedStyle(window)
+        self.style.set_theme("breeze")
         self.video_source = video_source
 
         #open vid source
@@ -16,8 +23,11 @@ class App:
         self.canvas = tkinter.Canvas(window, width=self.vid.width, height=self.vid.height)
         self.canvas.pack()
         
-        self.btn_snapshot = tkinter.Button(window, text="Snapshot", width=50, command=self.snapshot)
+        self.btn_snapshot = ttk.Button(window, text="Scan", width=50, command=self.snapshot)
         self.btn_snapshot.pack(anchor=tkinter.CENTER, expand=True)
+
+        self.stext = ScrolledText(window, width=50, height=10)
+        self.stext.pack(anchor=tkinter.CENTER, expand=True)
 
         self.delay = 15
         self.update()
@@ -40,6 +50,12 @@ class App:
 
         if ret:
             cv2.imwrite("frame-"+time.strftime("%d-%m-%Y-%H-%M-%S")+".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+            custom_config = r'--oem 3 --psm 6'
+            output_text = pytesseract.image_to_string(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR), config = custom_config)
+            print(output_text)
+            self.stext.insert(tkinter.INSERT, output_text)
+            self.stext.insert(tkinter.END, "___END___")
+            
 
 class VideoCapture:
     def __init__(self, video_source=0):
